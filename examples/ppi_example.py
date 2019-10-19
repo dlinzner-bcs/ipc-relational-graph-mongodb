8926eb6da0fc28b803c31ef884f0a133da18fb83
Matteo Manica <drugilsberg@gmail.com>
2019-10-19 18:51:34 +0200

    style: Moving scripts to examples.
    Signed-off-by: Matteo Manica <drugilsberg@gmail.com>

--- examples/ppi_example.py
+++ examples/ppi_example.py
@@ -8,8 +8,7 @@ from ipcrg.relations.relation import Relation
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-i', '--filepath', type=str, help='path to the edge list.',
-    required=True
+    '-i', '--filepath', type=str, help='path to the edge list.', required=True
 )
 parser.add_argument(
     '-m',
@@ -29,8 +28,6 @@ parser.add_argument(
 if __name__ == '__main__':
     # parse arguments
     args = parser.parse_args()
-    # parse edge list
-    df, proteins = parse_edge_list(args.filepath)
     # start the interface
     interface = MongoDBInterface(
         mongo_uri=args.mongo_uri, database_name=args.database_name
@@ -38,28 +35,12 @@ if __name__ == '__main__':
     # NOTE: start clean
     interface.delete_all_entities()
     interface.delete_all_relations()
-    # construct protein to entity mapping
-    protein_to_entity = {
-        protein: Protein(name=protein)
-        for protein in proteins
-    }
-    # create them in the relational graph
-    interface.create_entities(protein_to_entity.values())
-    # construct relations
-    relation_type = 'ppi'
-    columns = df.columns
-    relations = [
-        Relation(
-            source=protein_to_entity[source_protein],
-            target=protein_to_entity[target_protein],
-            relation_type=relation_type
-        )
-        for source_protein, target_protein in zip(df[columns[0]], df[columns[1]])
-    ]
-    # create them in the relational graph
-    interface.create_relations(relations)
+    # parse edge list an update the graph
+    interface.from_edge_list_filepath(args.filepath, Protein, 'ppi')
+    # parse proteins
+    proteins = [entity['name'] for entity in interface.get_all_entities()]
     # get adjacency
-    adjacency = interface.get_adjacency(proteins, relation_type)
+    adjacency = interface.get_adjacency(proteins, 'ppi')
     # look at it
     plt.matshow(adjacency.todense())
     plt.show()
